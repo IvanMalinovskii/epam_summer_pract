e88b037ac2b8737c53311a0ed74dd46ccf62e1eb
My Name <olgakaras@gmail.com>
2020-06-19 17:13:38 +0300

    + data base creation file

---
+++ sql/db_creator.sql
@@ -0,0 +1,34 @@
+create database play_base;
+
+use play_base;
+
+create table users(user_id int primary key auto_increment,
+				   user_login varchar(15) not null,
+                   user_password varchar(15) not null,
+                   user_email varchar(15) not null,
+                   user_phone varchar(12) not null,
+                   user_role enum('user', 'courier', 'admin') not null);
+
+create table authors(author_id int primary key auto_increment,
+					 author_name varchar(30) not null);
+
+create table genres(genre_id int primary key auto_increment,
+					 genre_name varchar(20) not null);
+
+create table plays(play_id int primary key auto_increment,
+				   play_name varchar(30) not null,
+                   play_description text not null,
+                   author_id int not null,
+                   genre_id int not null,
+                   foreign key(author_id) references authors(author_id) on delete cascade,
+                   foreign key(genre_id) references genres(genre_id) on delete cascade);
+                   
+create table dates(date_id int primary key auto_increment,
+				   date_day date not null,
+                   play_id int not null,
+                   foreign key(play_id) references plays(play_id));
+                   
+                   
+                   
+                   
+                   
